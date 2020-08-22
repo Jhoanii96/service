0efde304cf9d8969ab5bd4a407f1a7f51739e4f1
JhonA96 <jhoaniia96@gmail.com>
2020-08-22 01:39:44 -0500

    update myview

--- app/views/my/myView.php
+++ app/views/my/myView.php
@@ -18,6 +18,19 @@
 
     <link href="<?= FOLDER_PATH ?>/src/css/main.d810cf0ae7f39f28f336.css" rel="stylesheet">
 
+    <style>
+        .title-details {
+            color: #468847;
+            padding: 8px 35px 8px 14px;
+            margin-bottom: 20px;
+            text-shadow: 0 1px 0 rgba(255, 255, 255, 0.5);
+            background-color: #e3f3fc;
+            border: 1px solid #d9d5fb;
+            -webkit-border-radius: 4px;
+            -moz-border-radius: 4px;
+            border-radius: 4px;
+        }
+    </style>
 
 </head>
 
